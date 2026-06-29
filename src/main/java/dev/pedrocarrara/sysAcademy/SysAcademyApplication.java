50a3da59c8638f7439c5d74892e4a23031af658b
pedrocarrara12 <pepe.carrara@gmail.com>
2026-06-28 21:36:19 -0400

    estrutura inicial do projeto

---
+++ src/main/java/dev/pedrocarrara/sysAcademy/SysAcademyApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrocarrara.sysAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedrocarrara/sysAcademy/SysAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrocarrara.sysAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
